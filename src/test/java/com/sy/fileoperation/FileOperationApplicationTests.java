414918a139d28d67fbe4ef45f0f6bb8f4c8821fc
孙宇 <1870971964@qq.com>
2024-02-13 19:22:08 +0800

    first commit

---
+++ src/test/java/com/sy/fileoperation/FileOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.fileoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
